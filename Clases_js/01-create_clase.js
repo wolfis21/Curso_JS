716f7aa212f94d4c4815a839f3d9b5f0c378727f
Isaac <isaacsaado69@gmail.com>
2022-10-10 19:51:55 -0400

    new

--- Clases_js/01-create_clase.js
+++ Clases_js/01-create_clase.js
@@ -1,6 +1,11 @@
 // se implementa un constructor y en ese mismo momento se 
 // crean los atributos de la clase
 
+
+//No es posible crear objetos antes de declarar la clase
+//no se aplica el concepto de hoisting como las funciones
+
+//clase padre
 class Persona{
     constructor(nombre,apellido){
         this._nombre = nombre;
@@ -20,10 +25,27 @@ class Persona{
     set apellido(apellido){
         this._apellido = apellido;
     }
-};
+}
+// clase hija
+class Empleado extends Persona{
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido); //llamar al constructor de la clase padre
+        this._departamento = departamento;
+    }
+
+    get departamento(){
+        return this._departamento;
+    }
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
+}
 
 // Instancia de clase
 let persona1 = new Persona( 'Isaac', 'Saado');
-persona1.nombre = 'Alejandro mas' //set
-console.log(persona1.nombre); //get 
 
+// persona1.nombre = 'Alejandro mas' //set
+// console.log(persona1.nombre); //get 
+
+let empleado1 = new Empleado('Maria', 'perez','biblioteca');
+console.log(empleado1);
